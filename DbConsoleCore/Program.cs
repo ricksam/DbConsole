fbf74a6ff015d074566ca6f28b2930640bba985e
Ricardo Sampaio <ricardo.sampaio@bluecore.com.br>
2025-10-29 15:31:30 -0300

    dbconsole core

--- DbConsoleCore/Program.cs
+++ DbConsoleCore/Program.cs
@@ -2,37 +2,98 @@
 using System.Data;
 using System.Text.Json;
 using Microsoft.Data.SqlClient;
+using Oracle.ManagedDataAccess.Client;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using Npgsql;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
         // --- 1. Parameter Validation ---
-        if (args.Length != 2)
+        if (args.Length != 3)
         {
             Console.WriteLine("Error: Incorrect number of arguments.");
             Console.WriteLine("Usage: dotnet run <ConnectionString> <Query>");
             return;
+
+            // for test
+            /*args = new string[]{ 
+                "Oracle", 
+                "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=201.91.21.42)(PORT=1831)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=NBS)));User Id=BPANDA;Password=NBS;",
+                "SELECT CHAVE_ALTERNATIVA, PLACA, CHASSI_COMPLETO FROM NBS.CARRERAVW_PATIO_VEICULOS"};
+            */
         }
 
-        string connectionString = args[0];
-        string sqlQuery = args[1];
+        string dbType = args[0];
+        string connectionString = args[1];
+        string sqlQuery = args[2];
 
         // --- 2. Data Retrieval ---
         DataTable dataTable = new DataTable();
         try
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            if (dbType == "SqlServer")
             {
-                await connection.OpenAsync();
-
-                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
-                using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    // Load the results into a DataTable
-                    dataTable.Load(reader);
+                    await connection.OpenAsync();
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    using (DbDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        // Load the results into a DataTable
+                        dataTable.Load(reader);
+                    }
                 }
             }
+
+            if (dbType == "Oracle")
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    using (OracleCommand command = new OracleCommand(sqlQuery, connection))
+                    using (DbDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        // Load the results into a DataTable
+                        dataTable.Load(reader);
+                    }
+                }
+            }
+
+            if(dbType == "MySql")
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
+                    using (DbDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        // Load the results into a DataTable
+                        dataTable.Load(reader);
+                    }
+                }
+            }
+
+            if(dbType== "PostgreSql") {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                    using (DbDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        // Load the results into a DataTable
+                        dataTable.Load(reader);
+                    }
+                }
+            }
+            
         }
         catch (SqlException ex)
         {
@@ -70,7 +131,7 @@ public class Program
             foreach (var colName in columns)
             {
                 // Convert DBNull to null, otherwise use the value
-                object value = row[colName] == DBNull.Value ? null : row[colName];
+                object? value = row[colName] == DBNull.Value ? null : row[colName];
                 rowDict[colName] = value;
             }
             rows.Add(rowDict);
